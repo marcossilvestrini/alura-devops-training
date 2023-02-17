1957f5623bae5ad108e5058175e8d91280cd4986
marcossilvestrini <marcos.silvestrini@gmail.com>
2023-02-17 10:41:30 -0300

    Add get proccess

--- git/project/app.ps1
+++ git/project/app.ps1
@@ -7,3 +7,6 @@ Description: App writing in powershell language
 
 #Get Services
 Get-CimInstance -ClassName Win32_Service | Format-List
+
+#Get Proccess
+Get-CimInstance -ClassName Win32_Process | Format-List
