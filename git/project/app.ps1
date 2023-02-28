2e00373af3d6fdbf1a2b33f25491b815c553e81a
marcossilvestrini <marcos.silvestrini@gmail.com>
2023-02-28 08:22:06 -0300

    Add filter in get process

--- git/project/app.ps1
+++ git/project/app.ps1
@@ -8,5 +8,6 @@ Description: App writing in powershell language
 #Get Services
 Get-CimInstance -ClassName Win32_Service
 
-# GetProcess
-Get-CimInstance -ClassName Win32_Process
+# Get Process
+Get-CimInstance -ClassName Win32_Process -Filter "Name='code.exe'"
+
